7f85f929bf7ba8866780fb7822e8fee49f6407ed
wyxacl <wyxacl@gmail.com>
2020-01-16 15:34:13 +0800

    update sql

--- src/main/java/com/wyxacl/blog/server/entity/BaseDO.java
+++ src/main/java/com/wyxacl/blog/server/entity/BaseDO.java
@@ -4,9 +4,16 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * base data object
+ *
+ * @author wyx
+ * @version 1.0
+ * @since 2020-01-13 12:11
+ */
 @Data
 public abstract class BaseDO {
     private Long id;
     private LocalDateTime gmtCreate;
-    private LocalDateTime gmtUpdate;
+    private LocalDateTime gmtModified;
 }
